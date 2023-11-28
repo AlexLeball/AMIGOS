f4670c0204a6b88ef3331e40c34da27ba59ef248
alexandra perrin <alexandrarose1@live.com.au>
2023-11-28 14:15:55 +0100

    registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
         post 'accept'
         post 'decline'
       end
-  end
+    end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
