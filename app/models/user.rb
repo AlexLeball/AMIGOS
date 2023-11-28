7a06edd6b6ca1727869c920bdc5b73d14cd3aa7a
ahmed salhi <sirjaizz@gmail.com>
2023-11-28 10:35:54 +0100

    tables done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_many :events, through: :registrations
   has_many :registrations, dependent: :destroy
+  has_many :favorites
+  has_many :messages
+  has_many :user_categories
+  has_many :categories, through: :user_categories
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
