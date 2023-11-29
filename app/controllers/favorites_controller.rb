88d7e5d337cb138122d4f480d365e2676f6f8bf8
ahmed salhi <sirjaizz@gmail.com>
2023-11-29 12:20:20 +0100

    favorites complited

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,27 +2,16 @@ class FavoritesController < ApplicationController
   def create
     event = Event.find(params[:event_id])
 
-    if current_user.favorites.exclude?(event)
-      favorite = current_user.favorites.new(event: event)
-
+      favorite = Favorite.new(event: event, user: current_user)
       if favorite.save
         redirect_back(fallback_location: root_path, notice: 'Event added to favorites successfully.')
-      else
-        redirect_back(fallback_location: root_path, alert: 'There was an error adding the event to favorites.')
       end
-    else
-      redirect_back(fallback_location: root_path, alert: 'Event is already in favorites.')
-    end
   end
 
   def destroy
-    event = Event.find(params[:id])
-    if current_user.favorites.include?(event)
-      current_user.favorites.delete(event)
+    event = Event.find(params[:event_id])
+     Favorite.find(params[:id]).destroy
       redirect_back(fallback_location: root_path, notice: 'Event removed from favorites successfully.')
-    else
-      redirect_back(fallback_location: root_path, alert: 'Event is not in favorites.')
-    end
   end
 
   def index
