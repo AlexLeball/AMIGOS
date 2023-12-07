9a8d32b6246cd1b445d4a80db1a916f09b2e63d4
alexandra perrin <alexandrarose1@live.com.au>
2023-12-07 11:04:47 +0100

    toggle time

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,9 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
+  static targets = [ "togglableElement"]
+
   connect() {
-    console.log("hello");
+    console.log("hi");
   }
 
-
+  fire() {
+    (this.togglableElementTarget.style.height == "300px") ?
+    this.togglableElementTarget.style.height = "0px" : this.togglableElementTarget.style.height = "300px";
+  }
 }
